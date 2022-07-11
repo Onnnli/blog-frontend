38fd18e1cefa13ad810b613bd475bd6c34d6afd0
yelizaveta-aliantsevich <souslayk@gmail.com>
2022-07-11 11:43:49 +0300

    fixed token

--- src/axios.js
+++ src/axios.js
@@ -5,7 +5,9 @@ const instance = axios.create({
 });
 
 instance.interceptors.request.use((config) => {
-  config.headers.Authorization = localStorage.getItem('token');
+  const token = localStorage.getItem('token');
+
+  config.headers.Authorization = `Bearer ${token}`;
 
   return config;
 });
